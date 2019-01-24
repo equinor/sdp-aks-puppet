e841260fa17895f82e6c35ad40624f0dd3754acf
Boye Holden <BOYH@equinor.com>
2019-01-24 09:48:49 +0100

    Remove sentinel on reboot only

--- modules/compliance/manifests/night_sentinel.pp
+++ modules/compliance/manifests/night_sentinel.pp
@@ -1,16 +1,16 @@
 class compliance::night_sentinel {
   ## KURED REBOOT AT NIGHT ##
   cron { 'kured-night-reboot':
-    ensure => present,
+    ensure  => present,
     command => '[ -f "/var/run/reboot-required" ] && touch /var/run/reboot-required-night',
-    user => 'root',
-    minute => '0',
-    hour => '1'
+    user    => 'root',
+    minute  => '0',
+    hour    => '1'
   }
   cron { 'kured-night-sentinel-remove':
-    ensure => present,
-    command => '[ ! -f "/var/run/reboot-required" ] && [ -f "/var/run/reboot-required-night" ] && rm -rf /var/run/reboot-required-night',
-    user => 'root',
-    minute => '37'
+    ensure  => present,
+    command => '[ -f "/var/run/reboot-required-night" ] && rm -rf /var/run/reboot-required-night',
+    user    => 'root',
+    special => 'reboot'
   }
 }
