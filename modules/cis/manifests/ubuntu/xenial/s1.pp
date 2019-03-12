f265ec7fc0d8edc7593b354e1c5ef2f7879a48db
Boye Holden <BOYH@equinor.com>
2019-03-12 08:55:12 +0100

    Set up unattended upgrades. On upgrade mail should be sent to group

--- modules/cis/manifests/ubuntu/xenial/s1.pp
+++ modules/cis/manifests/ubuntu/xenial/s1.pp
@@ -1,4 +1,21 @@
 # Ubuntu CIS 16.04 Section #1
 class cis::ubuntu::xenial::s1 {
-  # Rules
+  # 1.8
+  package { 'unattended-upgrades':
+    ensure => 'present'
+  }
+  file { '/etc/apt/apt.conf.d/50unattended-upgrades':
+    ensure => 'file',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/cis/ubuntu/xenial/s1.8-50unattended-upgrades'
+  }
+  file { '/etc/apt/apt.conf.d/20auto-upgrades':
+    ensure => 'file',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/cis/ubuntu/xenial/s1.8-20auto-upgrades'
+  }
 }
