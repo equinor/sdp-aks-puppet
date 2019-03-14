14d9bfaac60545cf02dad8331db1dce0e696ec8b
esselt <boyh@equinor.com>
2019-03-14 13:52:35 +0100

    Remove duplicates

--- modules/cis/manifests/ubuntu/xenial.pp
+++ modules/cis/manifests/ubuntu/xenial.pp
@@ -1,5 +1,5 @@
 # Include all Ubuntu CIS 16.04 (Xenial)
 class cis::ubuntu::xenial {
-  $files = [ 's1', 's2', 's4', 's6.1' ]
+  $files = [ 's1', 's2', 's4', 's6' ]
   $files.each |$file| { include "cis::ubuntu::xenial::${file}" }
 }
