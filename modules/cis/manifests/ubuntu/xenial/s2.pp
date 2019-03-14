2f91c7bcc2322b6f790f401c5c3cf500e41a53ec
esselt <boyh@equinor.com>
2019-03-14 14:16:13 +0100

    Dynamic disabling/removal of service/packages

--- modules/cis/manifests/ubuntu/xenial/s2.pp
+++ modules/cis/manifests/ubuntu/xenial/s2.pp
@@ -1,10 +1,44 @@
 # Ubuntu CIS 16.04 Section #2
 class cis::ubuntu::xenial::s2 {
 
-  # 2.1.10
-  service { 'xinetd':
+  # 2.1.10, 2.2.4-14, 2.2.16-17
+  $services = [
+    'xinetd',
+    'avahi-daemon',
+    'cups',
+    'isc-dhcp-server',
+    'isc-dhcp-server6',
+    'slapd',
+    'nfs-server',
+    'rpcbind',
+    'bind9',
+    'vsftpd',
+    'apache2',
+    'dovecot',
+    'smbd',
+    'squid',
+    'snmpd',
+    'rsyncd',
+    'nis',
+  ]
+  service { $services:
     ensure => 'stopped',
     enable => false
+  }
+
+  # 2.1.11, 2.2.2, 2.3.1-5
+  $packages = [
+    'openbsd-inetd',
+    'xserver-xorg',
+    'nis',
+    'rsh-redone-client',
+    'rsh-client',
+    'talk',
+    'telnet',
+    'ldap-utils',
+  ]
+  package { $packages:
+    ensure => 'purged',
   }
 
   # 2.2.1.1 and 2.2.1.2
@@ -14,118 +48,5 @@ class cis::ubuntu::xenial::s2 {
       '-4 default kod nomodify notrap nopeer noquery',
       '-6 default kod nomodify notrap nopeer noquery'
     ]
-  }
-
-  # 2.2.3
-  service { 'avahi-daemon':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.4
-  service { 'cups':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.5
-  service { 'isc-dhcp-server':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.5
-  service { 'isc-dhcp-server6':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.6
-  service { 'slapd':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.7
-  service { 'nfs-server':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.7
-  service { 'rpcbind':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.8
-  service { 'bind9':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.9
-  service { 'vsftpd':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.10
-  service { 'apache2':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.11
-  service { 'dovecot':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.12
-  service { 'smbd':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.13
-  service { 'squid':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.14
-  service { 'snmpd':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.16
-  service { 'rsyncd':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.2.17
-  service { 'nis':
-    ensure => 'stopped',
-    enable => false
-  }
-  # 2.1.11
-  package { 'openbsd-inetd':
-    ensure => 'purged',
-  }
-  # 2.2.2
-  package { 'xserver-xorg':
-    ensure => 'purged',
-  }
-  # 2.3.1
-  package { 'nis':
-    ensure => 'purged',
-  }
-  # 2.3.2
-  package { 'rsh-redone-client':
-    ensure => 'purged',
-  }
-  # 2.3.2
-  package { 'rsh-client':
-    ensure => 'purged',
-  }
-  # 2.3.3
-  package { 'talk':
-    ensure => 'purged',
-  }
-  # 2.3.4
-  package { 'telnet':
-    ensure => 'purged',
-  }
-  # 2.3.5
-  package { 'ldap-utils':
-    ensure => 'purged',
   }
 }
