1ccc9dc66387105c808330b6214c82a25ed0a73b
esselt <boyh@equinor.com>
2019-04-23 15:16:32 +0200

    Add docker keyname to docker audit rules

--- modules/compliance/manifests/auditd.pp
+++ modules/compliance/manifests/auditd.pp
@@ -1,39 +1,15 @@
 class compliance::auditd {
 
-  auditd::rule { 'watch /usr/bin/docker':
-    content => '-w /usr/bin/docker -p wa',
+  auditd::rule { 'docker audit rules':
+    content => '-w /usr/bin/docker -p wa -k docker
+-w /var/lib/docker -p wa -k docker
+-w /etc/docker -p wa -k docker
+-w /lib/systemd/system/docker.service -p wa -k docker
+-w /lib/systemd/system/docker.socket -p wa -k docker
+-w /etc/default/docker -p wa -k docker
+-w /etc/docker/daemon.json -p wa -k docker
+-w /usr/bin/docker-containerd -p wa -k docker
+-w /usr/bin/docker-runc -p wa -k docker',
     order   => 101
-  }
-  auditd::rule { 'watch /var/lib/docker':
-    content => '-w /var/lib/docker -p wa',
-    order   => 102
-  }
-  auditd::rule { 'watch /etc/docker':
-    content => '-w /etc/docker -p wa',
-    order   => 103
-  }
-  auditd::rule { 'watch /lib/systemd/system/docker.service':
-    content => '-w /lib/systemd/system/docker.service -p wa',
-    order   => 104
-  }
-  auditd::rule { 'watch /lib/systemd/system/docker.socket':
-    content => '-w /lib/systemd/system/docker.socket -p wa',
-    order   => 105
-  }
-  auditd::rule { 'watch /etc/default/docker':
-    content => '-w /etc/default/docker -p wa',
-    order   => 106
-  }
-  auditd::rule { 'watch /etc/docker/daemon.json':
-    content => '-w /etc/docker/daemon.json -p wa',
-    order   => 107
-  }
-  auditd::rule { 'watch /usr/bin/docker-containerd':
-    content => '-w /usr/bin/docker-containerd -p wa',
-    order   => 108
-  }
-  auditd::rule { 'watch /usr/bin/docker-runc':
-    content => '-w /usr/bin/docker-runc -p wa',
-    order   => 109
   }
 }
