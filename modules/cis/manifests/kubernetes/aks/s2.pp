1b4430e3eba142ffeaf98de57d190d9835f72d57
Audun Solemdal <auls@equinor.com>
2019-06-20 12:42:24 +0200

    test class fixes

--- modules/cis/manifests/kubernetes/aks/s2.pp
+++ modules/cis/manifests/kubernetes/aks/s2.pp
@@ -1,5 +1,5 @@
 # Kubernetes CIS benchmark v1.3.0 section 2
-class cis::kubernetes::s2 {
+class cis::kubernetes::aks::s2 {
 
   # 2.2.9 and 2.2.10
   file { '/var/lib/kubelet/config.yaml':
--- modules/cis/manifests/kubernetes/kubernetes.pp
+++
@@ -1,6 +0,0 @@
-# Kubernetes CIS benchmark v1.3.0
-class cis::kubernetes 
-{
-  $files = [ 's2']
-  $files.each |$file| { include "cis::kubernetes::${file}" }
-}
