347176dafe866b701851fee5e35e98f8d680f3be
Audun Solemdal <auls@equinor.com>
2019-06-20 13:46:36 +0200

    fix kubernetes.pp file

--- modules/cis/manifests/kubernetes.pp
+++ modules/cis/manifests/kubernetes.pp
@@ -1,9 +1,4 @@
 # Include all Kubernetes CIS rules
 class cis::kubernetes {
-  if $facts['os']['name'] == 'Ubuntu' {
-    case $facts['os']['release']['full'] {
-      '16.04': { include cis::kubernetes::aks }
-      default: { notify("Unsupported version of Ubuntu ${facts['os']['release']['full']}") }
-    }
-  }
+include cis::kubernetes::aks
 }
